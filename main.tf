898c2002f7cf3a374cf79e8eea0a5b270f378ca7
Slav Donchev <slav.donchev@gmail.com>
2021-08-17 15:40:29 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "okta" {
 }
 
 terraform {
-  required_version = "~> 0.13.0"
+  required_version = ">= 0.13.0"
   required_providers {
     okta = {
       source = "providers.local/local/okta"
